d5062395f6ac6b750492629b70b2ac009d3dbb74
smtdmrll <smtdmrll@gmail.com>
2021-04-09 19:51:26 +0300

    ikinci commit

---
+++ src/day47_queue_map/C3_Maps01.java
@@ -0,0 +1,19 @@
+package day47_queue_map;
+
+import java.util.HashMap;
+
+public class C3_Maps01 {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer,String> objeMap =  new HashMap<>();
+		
+		objeMap.put(1001, "samet samet 2021 200tl");
+		objeMap.put(1002, "veli yan java");
+		objeMap.put(1003, "ali can c#");
+		System.out.println(objeMap);
+	}
+	
+	
+	
+}
